7b23d5186b53e3357102588551974d0e496d591b
Harn Hua Ng <harnhua@users.noreply.github.com>
2020-07-21 10:33:02 +0800

    Create intimeflow.tcl

---
+++ scripts/intime/simple/intimeflow.tcl
@@ -0,0 +1,9 @@
+# Sample script for InTime Optimization. Works in Windows & Linux environments
+# Adjust the run_target, runs_per_round and round variables according to your recipe
+flow reset
+flow set run_target local
+flow set runs_per_round 2
+flow set rounds 1
+flow set concurrent_runs 1
+flow set control_create_bitstreams false
+flow run_recipe "intime_default"
